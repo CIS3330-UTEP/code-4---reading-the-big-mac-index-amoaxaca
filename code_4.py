99940fd1a35e8a972a0142ae76737cac6a2fe52c
Alexa Oaxaca <amoaxaca@miners.utep.edu>
2023-02-24 00:39:45 -0700

    Revert "code_4"
    This reverts commit 47df8fbf55722ff2c1d73c686a775228207b30c6.

--- code_4.py
+++ code_4.py
@@ -1,53 +1,18 @@
-#import csv
+import csv
 import pandas as pd
 big_mac_file = './big-mac-full-index.csv'
-file = pd.read_csv(big_mac_file)
 
 def get_big_mac_price_by_year(year,country_code):
-    country = country_code
-    country=country.upper()
-    query= f"(iso_a3 == '{country}' and date >= '{year}-01-01'and date <= '{year}-12-31')"
-    country_df = file.query(query)
-    answer = round(country_df['dollar_price'].mean(),2)
-    return answer
-
+    pass # Remove this line and code your function
 
 def get_big_mac_price_by_country(country_code):
-    country=country_code
-    country=country.upper()
-    query= f"(iso_a3 == '{country}')"
-    country_df=file.query(query)
-    answer = round(country_df['dollar_price'].mean(),2)
-    return answer
-
-    
+    pass # Remove this line and code your function
 
 def get_the_cheapest_big_mac_price_by_year(year):
-    query= f"(date >= '{year}-01-01'and date <= '{year}-12-31')"
-    country_df=file.query(query)
-    min_idx=country_df['dollar_price'].idxmin()
-    price_min=file.loc[min_idx]
-    answer=(f"{price_min['name']}({price_min['iso_a3']}): ${round(price_min['dollar_price'],2)}")
-    return answer
-    
-
+    pass # Remove this line and code your function
 
 def get_the_most_expensive_big_mac_price_by_year(year):
-    query= f"(date >= '{year}-01-01'and date <= '{year}-12-31')"
-    country_df=file.query(query)
-    max_idx=country_df['dollar_price'].idxmax()
-    price_max=file.loc[max_idx]
-    answer=(f"{price_max['name']}({price_max['iso_a3']}): ${round(price_max['dollar_price'],2)}")
-    return answer
+    pass # Remove this line and code your function
+
 if __name__ == "__main__":
-    code_a=get_big_mac_price_by_year (2010,"arg")
-    print(code_a)
-
-    code_b=get_big_mac_price_by_country('mex')
-    print(code_b)
-
-    code_c=get_the_cheapest_big_mac_price_by_year('2008')
-    print(code_c)
-
-    code_d=get_the_most_expensive_big_mac_price_by_year('2014')
-    print(code_d)
+    pass # Remove this line and code your user interface
--- pandas_demo.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-filename='./big-mac-full-index.csv'
-df=pd.read_csv(filename)
-query_string = "(iso_a3=='ARG')"
-print(query_string)
-arg_df=df.query(query_string)
-print(arg_df)
-
